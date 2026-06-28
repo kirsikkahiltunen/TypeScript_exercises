34416ef01006347c9bcc2ed42f62dd833f4596ac
Kirsikka Hiltunen <kirsikka.hiltunen@gmail.com>
2026-06-28 19:32:49 +0300

    8. Checkup

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -27,26 +27,29 @@ app.get("/bmi", (req, res) => {
 
 app.post("/exercises", (req, res) => {
   const exercises = req.body.daily_exercises;
-  const target = Number(req.body.target);
+  const target = req.body.target;
 
   if (!exercises || !target) {
     return res.status(400).json({ error: "parameters missing" });
   }
 
+  if (!Array.isArray(exercises)) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+
   if (
     exercises.some((n: unknown) => isNaN(Number(n))) ||
-    !Array.isArray(exercises) ||
-    isNaN(target)
+    isNaN(Number(target))
   ) {
     return res.status(400).json({ error: "malformatted parameters" });
   } else {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    const result = calculateExercises(exercises, target);
+    const result = calculateExercises(exercises, Number(target));
     return res.json(result);
   }
 });
 
-const PORT = 3003;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
